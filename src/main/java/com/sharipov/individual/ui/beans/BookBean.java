93022a34a11db3f2136fb55250d04051b52fe90d
Pavel Sharipov <pavel.sharipov@gmail.com>
2017-05-19 14:59:52 +0300

    ui BookBean.java added problems with webapp

---
+++ src/main/java/com/sharipov/individual/ui/beans/BookBean.java
@@ -0,0 +1,25 @@
+package com.sharipov.individual.ui.beans;
+
+
+import com.sharipov.individual.dao.BookDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+/**
+ * Created with IntelliJ IDEA
+ * Author: Pavel Sharipov
+ * Date: 19.05.2017
+ * Time: 14:25
+ */
+
+@Named("book")
+@ViewScoped
+public class BookBean {
+
+    @Autowired
+    private BookDAO bookDAO;
+
+
+}
