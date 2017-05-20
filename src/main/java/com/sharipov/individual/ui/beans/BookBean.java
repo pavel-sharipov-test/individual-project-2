d908508693a315b678c8a712d3b5abf3cf4a6ca8
Pavel Sharipov <pavel.sharipov@gmail.com>
2017-05-20 23:13:21 +0300

    primefaces added, table of books without add/delete functionality

--- src/main/java/com/sharipov/individual/ui/beans/BookBean.java
+++ src/main/java/com/sharipov/individual/ui/beans/BookBean.java
@@ -2,10 +2,13 @@ package com.sharipov.individual.ui.beans;
 
 
 import com.sharipov.individual.dao.BookDAO;
+import com.sharipov.individual.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA
@@ -14,7 +17,7 @@ import javax.inject.Named;
  * Time: 14:25
  */
 
-@Named("book")
+@Named("bookBean")
 @ViewScoped
 public class BookBean {
 
@@ -24,6 +27,22 @@ public class BookBean {
     public String getBookName() {
         return bookDAO.find(1).getName();
     }
+
+    @PostConstruct
+    private void init() {
+        books = bookDAO.findAll();
+    }
+
+    private List<Book> books;
+
+ /*   public void deleteBook(Book book) {
+        bookDAO.delete(book);
+    }*/
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
 
 
 }
